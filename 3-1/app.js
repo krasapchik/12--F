db0093ba1ee88c74928820b467f90a1e4f29a394
krasapchik <kutman_mamytov359@gmail.com>
2023-01-22 16:21:40 +0600

    homwr2

---
+++ 3-1/app.js
@@ -0,0 +1,31 @@
+var M = 'Friday'
+switch(M) {
+    case 'Manday':
+        case 'manday':
+            console.log('Понедельник');
+            break;
+    case 'Tuesday':
+        case 'tuesday':
+            console.log('Вторник');
+            break;
+    case 'Wednesday':
+        case 'wednesday':
+            console.log('Среда');
+            break;
+    case 'Thursday':
+        case 'thursday':
+            console.log('Четверг');
+            break;
+    case 'Friday':
+        case 'friday':
+            console.log('Пятница');
+            break;
+    case 'Saturday':
+        case 'saturday':
+            console.log('Суббота');
+            break;
+    case 'Sunday':
+        case 'sunday':
+            console.log('Воскресенье');
+            break;
+}
